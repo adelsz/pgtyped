070049171d7bd0b91ff5171ab102477be17ed0c2
Christian Mösl <christian.moesl@live.at>
2021-04-26 14:04:56 +0200

    Add CLI flags `--watch` and `--file` as defined in help message

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -185,8 +185,10 @@ if (require.main === module) {
     process.exit(0);
   }
 
-  const { c: configPath, w: isWatchMode, f: fileOverride } = args;
-
+  const isWatchMode = args.w || args.watch
+  const fileOverride = args.f || args.file
+  const configPath = args.c
+    
   if (typeof configPath !== 'string') {
     console.log('Config file required. See help -h for details.\nExiting.');
     process.exit(0);
