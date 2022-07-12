31c614a72b0bdfa75db30ded98b273d70380c4eb
Logan Glasson <logan+github.com@glasson.nz>
2022-07-13 06:19:24 +1200

    fix: emit Json type when required by JsonArray (#456)
    * fix: emit Json type when required by JsonArray
    
    Fixes #323
    
    * Make linter happy
    
    Co-authored-by: Adel Salakh <adel.salakh@gmail.com>

--- packages/cli/src/types.test.ts
+++ packages/cli/src/types.test.ts
@@ -8,4 +8,17 @@ describe('TypeAllocator', () => {
     });
     expect(types.use('foo')).toEqual('bar');
   });
+
+  // Covers issue #323
+  test('Uses `Json` when using `JsonArray`', () => {
+    const types = new TypeAllocator(DefaultTypeMapping);
+    // `_json` is the type name from PG corresponding to an array of JSON values
+    types.use('_json');
+    // The definition of `JsonArray` depends on the definition of `Json`, so we
+    // expect both to be included
+    expect(types.types).toMatchObject({
+      Json: expect.objectContaining({ name: 'Json' }),
+      JsonArray: expect.objectContaining({ name: 'JsonArray' }),
+    });
+  });
 });
